9b1f3314ebb61e1da2a88289d7a5929d56fb426e
d10w <demitri.jerow@gmail.com>
2018-10-10 22:24:27 -0400

    adding original workbook to github

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Wandr';
+myHeading.style.color="#3b8094";
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/wanderlust-home.jpg') {
+      myImage.setAttribute ('src','images/fjord.jpg');
+    } else {
+      myImage.setAttribute ('src','images/wanderlust-home.jpg');
+    }
+}
+
+var priceUSA = document.querySelector ('h2')
+priceUSA.textContent = "USA Currency Exchange"
